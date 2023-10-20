7531db56e7e9b537094e981dbbaab3ed8629b7af
dieego17 <diegorubio1704.drs@gmail.com>
2023-10-20 19:33:15 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let imagenes_animales = ["cabra.jpg", "cerdo.jpg", "conejo.png", "conejo2.jpg", "cordero.png", "elefante.jpg", "erizo.jpg", "gato.png", "jirafa.png", "leon.png", "mono.jpg", "oso.jpg", "osoblanco.png", "osopanda.png", "pato.png", "perro.png", "pollo.png", "pradera.jpg", "tigre.jpg", "vaca.png", "tucan.jpg", "loro.jpg", "cigueña.jpg", "foca.jpg", "koala.jpg", "cebra.jpg", "canguro.jpg", "pinguino.jpg", "zorro.jpg", "potro.jpg", "tortuga.jpg", "lobo.jpg"];
+
+let imagenes_cartas = ["atras1.jpg", "atras2.jpg", "atras3.jpg", "atras4.jpg", "atras5.jpg", "atras6.jpg", "atras7.jpg", "atras8.jpg"];
+
+let header_title = document.getElementById("header_title");
+let configurador = document.getElementById("configurador");
+let juego = document.getElementById("juego");
+let tiempo = document.getElementById("tiempo");
+let ayuda = document.getElementById("ayuda");
+let botones = document.getElementById("botones");
+let finjuego = document.getElementById("finjuego");
+
+let configuracion_tama = document.getElementById("configuracion_tama");
+let configuracion_cartas = document.getElementById("configuracion_cartas");
+let configuracion_juego = document.getElementById("configuracion_juego");
+let configuracion_ayuda = document.getElementById("configuracion_ayuda");
+
+let config_card_body = document.getElementById("config_card_body");
+let visor_imagenes_juego = document.getElementById("visor_imagenes_juego");
+let visor_marcador = document.getElementById("visor_marcador");
+let visor_tiempo = document.getElementById("visor_tiempo");
+let visor_ayuda = document.getElementById("visor_ayuda");
+
+let btn_comprobar = document.getElementById("btn_comprobar");
+let btn_validar = document.getElementById("btn_validar");
+let btn_nueva_partida = document.getElementById("btn_nueva_partida");
+
+
+let imagenes = document.getElementsByTagName("img");
